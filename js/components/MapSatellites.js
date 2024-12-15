3447be7999e37e3f2d817560de3ddde7faee7914
llada60 <572192783@qq.com>
2024-12-15 10:52:44 +0100

    20241215 Animation Update
    change current time according to launchLog.

--- js/components/MapSatellites.js
+++ js/components/MapSatellites.js
@@ -8,24 +8,32 @@ export function display2DSatellites() {
         const currentEntities = ctx.view2D.entities;
         currentEntities.removeAll();
         const launchDatas = data_preproceess(ctx.LAUNCHLOG.DATA, ctx.SITES.DATA);
+        const lifespan = 2000;
         ctx.view3D.clock.onTick.addEventListener(() => {
             // Load the satellite
-            const currentTime = Cesium.JulianDate.toDate(ctx.view3D.clock.currentTime); // 当前时间
+            const currentTime = Cesium.JulianDate.toDate(ctx.view3D.clock.currentTime); 
             launchDatas.forEach((launchData) => {
                 const launchDate = launchData.Launch_Date; // lanch time
                 const loc = launchData.loc; // longtitude and lattitude
 
                 if (currentTime.toDateString() === launchDate.toDateString()) {
-                    const launchEntity = createSatelliteEntity(loc, launchDate);
+                    const launchRadiusEntity = createSatelliteRadiusEntity(loc, launchDate, lifespan);
+                    // const launchEntity = createSatelliteEntity(loc);
                     // 5seconds remove the point
-                    if (launchEntity){
-                        var entity_id = currentEntities.add(launchEntity);
+                    if (launchRadiusEntity){
+                        var entity_id = currentEntities.add(launchRadiusEntity);
                     }
+                    // if (launchEntity){
+                    //     var entity2_id = currentEntities.add(launchEntity);
+                    // }
                     setTimeout(() => {
+                        // if (entity2_id){
+                        //     currentEntities.remove(entity2_id);
+                        // }
                         if (entity_id){
                             currentEntities.remove(entity_id);
                         }
-                    }, 1000);
+                    }, lifespan);
                 }
             });
         });
@@ -36,15 +44,42 @@ export function display2DSatellites() {
     }
 }
 
-function createSatelliteEntity(loc, launchDate){
+// function createSatelliteEntity(loc){
+//     const startRadius = 5.0;
+//     return {
+//         position: Cesium.Cartesian3.fromDegrees(loc.Longitude, loc.Latitude),
+//         point: {
+//             pixelSize: startRadius,
+//             color: Cesium.Color.RED,
+//             outlineColor: Cesium.Color.RED,
+//             outlineWidth: 0,
+//         },
+//     }
+// }
+
+function createSatelliteRadiusEntity(loc, launchDate, lifespan){
+    const startRadius = 5.0;
+    const maxRadius = 60.0;
+    const startAlpha = 0.1;
+    const startTime = new Date();
     return {
         position: Cesium.Cartesian3.fromDegrees(loc.Longitude, loc.Latitude),
         point: {
-            pixelSize: 5,
+            pixelSize: new Cesium.CallbackProperty((time) => {
+                const elapsed = (new Date() - startTime);
+                const progress = elapsed / lifespan;
+
+                if (progress >= 1) return 0; 
+                if (progress <= 0.0) return startRadius;
+                return startRadius + progress * (maxRadius - startRadius); 
+            }, false),
             color: new Cesium.CallbackProperty((time) => {
-                // alpha
-                const alpha = 1;
-                return Cesium.Color.RED.withAlpha(alpha);
+                const elapsed = (new Date() - startTime);
+                const progress = elapsed / lifespan;
+                if (progress>=1.0) return Cesium.Color.RED.withAlpha(0);
+                if (progress<=0.0) return Cesium.Color.RED.withAlpha(1);
+                // return Cesium.Color.RED.withAlpha(0.1);
+                return Cesium.Color.RED.withAlpha(startAlpha * (1.0 - progress));
             }, false),
         },
     };
