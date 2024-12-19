799bc289680e533525f03b027aa25d5d4f1cd39e
llada60 <572192783@qq.com>
2024-12-19 14:55:50 +0100

    20241219 bugCountry

--- js/components/MapCountry.js
+++ js/components/MapCountry.js
@@ -2,27 +2,37 @@
 import * as Cesium from "cesium";
 import {ctx} from "/js/utils/config";
 
-function update2DCountry(currentEntities, num_sc){
-    currentEntities.removeAll();
+function removeCountry(currentEntities, arr){
+    // console.log(arr);
+    for (let i = 0; i < arr.length; i++) { 
+        currentEntities.remove(arr[i]);   
+    }
+    return null;
+}
+
+function update2DCountry(currentEntities, num_sc, exist_country){
+    // setTimeout(() => {
+    removeCountry(currentEntities, exist_country);
+    // }, 1000);
+    
     const country_loc = ctx.COUNTRY.DATA;
+    let arr = [];
+    let entity_id;
     for (let key in num_sc) {
         let loc = country_loc[key];
-        const launchRadiusEntity = createSatelliteRadiusEntity(loc, num_sc[key]);
-        if (launchRadiusEntity){
-            let entity_id = currentEntities.add(launchRadiusEntity);
-        }
+        entity_id = createSatelliteRadiusEntity(currentEntities, loc, num_sc[key], key);
+        arr.push(entity_id);
     }
+    return arr;
 }
 
 
 export function display2DCountry() {
     try {
         const currentEntities = ctx.view2D.entities;
-        // ctx.currentSiteEntity.removeAll();
-        currentEntities.removeAll();
-        
+        let exist_country = [];
         setInterval(() => {
-            update2DCountry(currentEntities, ctx.NUM_SC);
+            exist_country = update2DCountry(currentEntities, ctx.NUM_SC, exist_country);
         }, 1000);
 
     } catch (error) {
@@ -31,15 +41,46 @@ export function display2DCountry() {
 }
 
 
-function createSatelliteRadiusEntity(loc, size){
-    return {
-        position: Cesium.Cartesian3.fromDegrees(loc.Longitude, loc.Latitude),
-        point: {
-            pixelSize: size,
-            color: Cesium.Color.GREY.withAlpha(0.3),
-            outlineColor: Cesium.Color.GREY.withAlpha(0.3),
-        },
-    };
+function createSatelliteRadiusEntity(currentEntities, loc, size, key){
+    const imagePath = `public/img/flags/${key}.png`;
+    let entity_id;
+
+
+    fetch(imagePath,{ method:'HEAD' })
+        .then((response) => {
+            if (response.ok) {
+                entity_id = currentEntities.add({
+                    position: Cesium.Cartesian3.fromDegrees(loc.Longitude, loc.Latitude),
+                    billboard: {
+                        image: imagePath,
+                        width: size ,  // Adjust size for better visibility
+                        height: size,
+                        color: new Cesium.Color(1.0, 1.0, 1.0, 0.5)
+                    },
+                });
+            } else {
+                entity_id = currentEntities.add({
+                    position: Cesium.Cartesian3.fromDegrees(loc.Longitude, loc.Latitude),
+                    point: {
+                        pixelSize: size,
+                        color: Cesium.Color.GREY.withAlpha(0.3),
+                        outlineColor: Cesium.Color.GREY.withAlpha(0.3),
+                    }
+                });
+            }
+        })  
+        .catch(() => {
+            entity_id = currentEntities.add({
+                position: Cesium.Cartesian3.fromDegrees(loc.Longitude, loc.Latitude),
+                point: {
+                    pixelSize: size,
+                    color: Cesium.Color.GREY.withAlpha(0.3),
+                    outlineColor: Cesium.Color.GREY.withAlpha(0.3),
+                }
+            }); // Add default point on error
+    });
+    // console.log(entity_id);
+    return entity_id;
 }
 
 
